c33316389e3bffe85816cc11297128f8c70ca21b
Dani <94472881+suri0702@users.noreply.github.com>
2024-09-18 14:52:19 -0300

    Add files via upload

---
+++ Lab_32/js/questions.js
@@ -0,0 +1,80 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Qual foi o primeiro computador pessoal vendido em larga escala?",
+    answer: "Altair 8800",
+    options: [
+      "Apple I",
+      "Commodore PET",
+      "IBM PC",
+      "Altair 8800"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Em que ano foi criado o primeiro vírus de computador?",
+    answer: "1971",
+    options: [
+      "1982",
+      "1971",
+      "1990",
+      "1986"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual era a principal função da ARPANET, precursor da internet?",
+    answer: "Compartilhar informações militares",
+    options: [
+      "Conectar universidades",
+      "Transferir arquivos de vídeo",
+      "Compartilhar informações militares",
+      "Criar redes sociais"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual é a linguagem de programação criada por Guido van Rossum em 1991?",
+    answer: "Python",
+    options: [
+      "Java",
+      "C#",
+      "Ruby",
+      "Python"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual é o nome do primeiro computador de uso geral programável?",
+    answer: "ENIAC",
+    options: [
+      "ENIAC",
+      "UNIVAC",
+      "Colossus",
+      "Z3"
+    ]
+  },
+{
+numb: 6,
+question: "Qual é o nome do primeiro navegador da web?",
+answer: "Mosaic",
+options: [
+  "Internet Explorer",
+  "Netscape Navigator",
+  "Mosaic",
+  "Safari"
+]
+},
+
+  {
+  numb: 7,
+     question: "Qual é o principal objetivo do protocolo TCP/IP",
+     answer: "Facilitar a comunicação entre dispositivos em redes",
+     options: [
+       "Proteger dados",
+       "Controlar redes locais",
+       "Facilitar a comunicação entre dispositivos em redes",
+       "Armazenar dados em nuvem"
+     ]
+   },
+];
